f6e859d93b316ebc8705970abea4d3aa11c97550
mavenraven.org <developers@mavenraven.org>
2014-03-12 20:29:46 -0400

    Adds initial project for chronic-haskell.

---
+++ src/Parsing/Chronic.hs
@@ -0,0 +1,18 @@
+module Parsing.Chronic
+{-
+    ( _areaCode
+    , f
+    , g
+    )
+-}
+    where
+
+import Text.ParserCombinators.Parsec.Char 
+import Text.Parsec.Prim 
+import Text.ParserCombinators.Parsec.Combinator
+
+f :: Bool
+f = False
+
+g :: Bool
+g = False
